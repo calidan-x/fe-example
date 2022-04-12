a568836ea9c8a5a2abc5b6f585e23324074b5189
Calidan <calidan@vip.qq.com>
2022-04-12 11:14:30 +0800

    update code

--- src/pages/input-test/input-test.tsx
+++ src/pages/input-test/input-test.tsx
@@ -1,10 +1,13 @@
+import { useState } from 'react'
+
 import { Input } from '@/components'
 
 export const InputTest = () => {
+  const [text, setText] = useState('')
   return (
     <div>
       <input value="没法改变的文字" />
-      <Input />
+      <Input value={text} onValueChange={setText} />
     </div>
   )
 }
